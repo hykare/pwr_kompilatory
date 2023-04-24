8110bafd50350e196a3fd56f619c2d8120fb3d55
hykare <78385964+hykare@users.noreply.github.com>
2023-04-25 00:34:43 +0200

    refactor for testing

--- zad_2.rb
+++ zad_2.rb
@@ -1,12 +1,4 @@
-while true
-  puts 'enter ip address to validate:'
-  ip_address = gets.chomp
-
-  valid = ip_address =~ /^(25[0-5]|2[0-4][0-9]|1[0-9]?[0-9]?|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/
-  unless valid.nil?
-    puts '✓ valid'
-  else
-    puts '✗ invalid'
-  end
-  puts
+def valid_ip?(ip_address)
+  match = ip_address =~ /^(25[0-5]|2[0-4][0-9]|1[0-9]?[0-9]?|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/
+  !match.nil?
 end
