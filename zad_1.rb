81324608477542d8765b5f6f18397737af2c1ef9
hykare <78385964+hykare@users.noreply.github.com>
2023-04-24 22:40:56 +0200

    refactor for tests

--- zad_1.rb
+++ zad_1.rb
@@ -1,13 +1,4 @@
-while true
-  puts 'enter binary number'
-  number = gets.chomp
-
-  power_of_two = number =~ /\b0*10*\b/
-
-  unless power_of_two.nil?
-    puts 'power of 2'
-  else
-    puts 'not power of 2'
-  end
-  puts
+def power_of_two?(number)
+  match = number =~ /\b0*10*\b/
+  !match.nil?
 end
